b0d2bd08ca533ef2e26b3fae791165f4f6069f27
yanyan-cas <yanyan.cas@gmail.com>
2018-05-02 22:48:36 +0800

    Isomap and MDS
    An demo of MDS added, needs modification~

---
+++ Isomap/isomap.m
@@ -0,0 +1,4 @@
+%ISOMAP Runs the Isomap algorithm
+%   [mappedX, mapping] = isomap(X, no_dims, k); 
+% of this component are returned in mapping.conn_comp.
+% This file is part of the Matlab Toolbox for Dimensionality Reduction.
